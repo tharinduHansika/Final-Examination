1a93227cec2e14f2448f50a7761db6b49c226cf3
tharinduHansika <tharindunirma@gmail.com>
2022-07-12 10:54:07 +0530

    Created save method and update method

--- IJSE/src/controller/StudentFormController.java
+++ IJSE/src/controller/StudentFormController.java
@@ -5,6 +5,7 @@ import com.jfoenix.controls.JFXTextField;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -31,7 +32,7 @@ public class StudentFormController {
     public JFXTextField txtAddress;
     public JFXTextField txtNIC;
 
-    public void initialize(){
+    public void initialize() {
         colStudentID.setCellValueFactory(new PropertyValueFactory<>("student_id"));
         colStudentName.setCellValueFactory(new PropertyValueFactory<>("student_name"));
         colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
@@ -46,13 +47,29 @@ public class StudentFormController {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+
+        tblStudent.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue!=null){
+                setData((Student)newValue);
+                btnSave.setText("Update");
+            }
+        });
+    }
+
+    private void setData(Student newValue) {
+        txtStudentID.setText(newValue.getStudent_id());
+        txtStudentName.setText(newValue.getStudent_name());
+        txtEmail.setText(newValue.getEmail());
+        txtContact.setText(newValue.getEmail());
+        txtAddress.setText(newValue.getAddress());
+        txtNIC.setText(newValue.getNic());
     }
 
     private void loadAllStudent() throws SQLException, ClassNotFoundException {
         ResultSet resultSet = CrudUtil.execute("SELECT * FROM Student");
         ObservableList<Student> obList = FXCollections.observableArrayList();
 
-        while (resultSet.next()){
+        while (resultSet.next()) {
             obList.add(
                     new Student(
                             resultSet.getString("student_id"),
@@ -73,9 +90,44 @@ public class StudentFormController {
 
     public void btnSaveOnAction(ActionEvent actionEvent) {
         tblStudent.refresh();
-        Student student =  new Student(txtStudentID.getText(),txtStudentName.getText(),txtEmail.getText(),txtContact.getText(), txtAddress.getText(),txtNIC.getText());
+        Student student = new Student(txtStudentID.getText(), txtStudentName.getText(), txtEmail.getText(), txtContact.getText(), txtAddress.getText(), txtNIC.getText());
 
-        
+        try {
+            if (btnSave.getText().equals("Save")) {
+                if (CrudUtil.execute("INSERT INTO Student VALUES(?,?,?,?,?,?)", student.getStudent_id(), student.getStudent_name(), student.getEmail(), student.getContact(),
+                        student.getAddress(), student.getNic())) {
+                    new Alert(Alert.AlertType.CONFIRMATION, "Saved!").show();
+                    loadAllStudent();
+                    clearTextField();
 
+                } else {
+                    new Alert(Alert.AlertType.ERROR, "Error!").show();
+                }
+            } else {
+                if (CrudUtil.execute("UPDATE Student SET student_name=?,email=?,contact=?,address=?,nic=? WHERE student_id=?",
+                        student.getStudent_name(),student.getEmail(), student.getContact(), student.getAddress(),student.getNic(),student.getStudent_id())){
+                    new Alert(Alert.AlertType.CONFIRMATION,"Updated!").show();
+                    tblStudent.refresh();
+                    loadAllStudent();
+
+                }else {
+                    new Alert(Alert.AlertType.ERROR,"Error!").show();
+                }
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void clearTextField() {
+        txtStudentID.clear();
+        txtStudentName.clear();
+        txtEmail.clear();
+        txtContact.clear();
+        txtAddress.clear();
+        txtNIC.clear();
     }
 }
