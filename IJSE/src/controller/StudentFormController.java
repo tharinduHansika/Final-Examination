24af89a1cb007477ff23c820d2b1d0eb64caa2e8
tharinduHansika <tharindunirma@gmail.com>
2022-07-12 10:32:20 +0530

    Created loadAllStudent with initialize method

--- IJSE/src/controller/StudentFormController.java
+++ IJSE/src/controller/StudentFormController.java
@@ -2,10 +2,17 @@ package controller;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import model.Student;
+import util.CrudUtil;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class StudentFormController {
     public TableView<Student> tblStudent;
@@ -24,9 +31,51 @@ public class StudentFormController {
     public JFXTextField txtAddress;
     public JFXTextField txtNIC;
 
+    public void initialize(){
+        colStudentID.setCellValueFactory(new PropertyValueFactory<>("student_id"));
+        colStudentName.setCellValueFactory(new PropertyValueFactory<>("student_name"));
+        colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        colContact.setCellValueFactory(new PropertyValueFactory<>("contact"));
+        colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+        colNIC.setCellValueFactory(new PropertyValueFactory<>("nic"));
+
+        try {
+            loadAllStudent();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadAllStudent() throws SQLException, ClassNotFoundException {
+        ResultSet resultSet = CrudUtil.execute("SELECT * FROM Student");
+        ObservableList<Student> obList = FXCollections.observableArrayList();
+
+        while (resultSet.next()){
+            obList.add(
+                    new Student(
+                            resultSet.getString("student_id"),
+                            resultSet.getString("student_name"),
+                            resultSet.getString("email"),
+                            resultSet.getString("contact"),
+                            resultSet.getString("address"),
+                            resultSet.getString("nic")
+                    )
+            );
+
+            tblStudent.setItems(obList);
+        }
+    }
+
     public void btnDeleteOnAction(ActionEvent actionEvent) {
     }
 
     public void btnSaveOnAction(ActionEvent actionEvent) {
+        tblStudent.refresh();
+        Student student =  new Student(txtStudentID.getText(),txtStudentName.getText(),txtEmail.getText(),txtContact.getText(), txtAddress.getText(),txtNIC.getText());
+
+        
+
     }
 }
