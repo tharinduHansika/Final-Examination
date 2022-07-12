85c1eb93229e17ac0b706a6a4e12f24ee2aa33fd
tharinduHansika <tharindunirma@gmail.com>
2022-07-12 10:15:12 +0530

    Setted UI IDs and OnAction Methods

--- IJSE/src/controller/StudentFormController.java
+++ IJSE/src/controller/StudentFormController.java
@@ -1,4 +1,32 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import model.Student;
+
 public class StudentFormController {
+    public TableView<Student> tblStudent;
+    public TableColumn colStudentID;
+    public TableColumn colStudentName;
+    public TableColumn colEmail;
+    public TableColumn colContact;
+    public TableColumn colAddress;
+    public TableColumn colNIC;
+    public JFXButton btnDelete;
+    public JFXButton btnSave;
+    public JFXTextField txtStudentID;
+    public JFXTextField txtStudentName;
+    public JFXTextField txtEmail;
+    public JFXTextField txtContact;
+    public JFXTextField txtAddress;
+    public JFXTextField txtNIC;
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
 }
